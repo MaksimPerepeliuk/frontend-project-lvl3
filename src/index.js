a266bc579d4a231af6280ddea424edaa155db025
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2022-11-21 18:59:27 +0300

    update webpack configuration file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-// import '../index.css';
+import '../index.css';
 // import test_img from '../assets/image.jpg';
+// import '../index.html';
+console.log('hello56f');
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
   },
   devServer: {
-    static: './build',
+    static: './',
     port: 8080,
     open: true,
   },
